df2b0f54ca789282e206465a63bd5fde238a338d
Guilherme Abel <guilhermeabel8@gmail.com>
2024-03-09 23:12:47 -0300

    feat: testando otimizacoes sql

--- init.sql
+++ init.sql
@@ -4,86 +4,52 @@ CREATE TABLE IF NOT EXISTS clientes (
     saldo INTEGER NOT NULL DEFAULT 0
 );
 
-CREATE UNLOGGED TABLE IF NOT EXISTS transacoes (
-    id SERIAL PRIMARY KEY,
-    idCliente INTEGER NOT NULL,
-    valor INTEGER NOT NULL,
-    tipo CHAR(1) NOT NULL,
-    descricao VARCHAR(10) NOT NULL,
-    dataCriacao TIMESTAMP NOT NULL
-);
-
 INSERT INTO clientes (limite)
 VALUES
-  (1000 * 100),
-  (800 * 100),
-  (10000 * 100),
-  (100000 * 100),
-  (5000 * 100);
+  (100000),
+  (80000),
+  (1000000),
+  (10000000),
+  (500000);
 
-ALTER TABLE transacoes
-SET (autovacuum_enabled = false);
+CREATE INDEX idCliente_idx ON clientes (id);
 
-CREATE INDEX idx_transacoes ON transacoes (idCliente ASC);
+DO $$ 
+DECLARE
+    idCliente INT;
+BEGIN
+    FOR idCliente IN (select distinct clientes.id from clientes) LOOP
+        EXECUTE FORMAT('
+			CREATE UNLOGGED TABLE "transacoes_%s" (
+				id serial,
+				valor int NOT NULL,
+				tipo varchar(1) NOT NULL,
+				descricao varchar(10) NOT NULL,
+				dataCriacao timestamp DEFAULT now()
+			);
+			CREATE INDEX transacoes_cliente_%s_idx ON "transacoes_%s" (id DESC);
+        ', idCliente, idCliente, idCliente);
+    END LOOP;
+END $$;
 
-CREATE OR REPLACE FUNCTION debitar(
-    idClienteTx INTEGER,
-    valorTx INT,
-    descricaoTx VARCHAR(10))
-RETURNS TABLE (
-    novoSaldo INT,
-    sucesso BOOL,
-    limite INT)
+CREATE OR REPLACE
+PROCEDURE delete_old_transacoes()
 LANGUAGE plpgsql
 AS $$
 DECLARE
-    saldoAtual INT;
-    limiteAtual INT;
+	idCliente INT;
+	tableName TEXT;
 BEGIN
-    PERFORM pg_advisory_xact_lock(idClienteTx);
-
-    SELECT 
-        clientes.limite,
-        clientes.saldo
-    INTO
-        limiteAtual,
-        saldoAtual
-    FROM clientes
-    WHERE id = idClienteTx;
-
-    IF saldoAtual - valorTx >= limiteAtual * -1 THEN
-        INSERT INTO transacoes VALUES(DEFAULT, idClienteTx, valorTx, 'd', descricaoTx, NOW());
-        
-        RETURN QUERY
-        UPDATE clientes 
-        SET saldo = saldo - valorTx 
-        WHERE id = idClienteTx
-        RETURNING saldo, TRUE, limiteAtual;
-    ELSE
-        RETURN QUERY SELECT saldoAtual, FALSE, limiteAtual;
-    END IF;
-END;
-$$;
-
-CREATE OR REPLACE FUNCTION creditar(
-    idClienteTx INTEGER,
-    valorTx INT,
-    descricaoTx VARCHAR(10))
-RETURNS TABLE (
-    novoSaldo INT,
-    sucesso BOOL,
-    limiteAtual INT)
-LANGUAGE plpgsql
-AS $$
-BEGIN
-    PERFORM pg_advisory_xact_lock(idClienteTx);
-
-    INSERT INTO transacoes VALUES(DEFAULT, idClienteTx, valorTx, 'c', descricaoTx, NOW());
-
-    RETURN QUERY
-        UPDATE clientes
-        SET saldo = saldo + valorTx
-        WHERE id = idClienteTx
-        RETURNING saldo, TRUE, clientes.limite;
-END;
-$$;
+	FOR idCliente IN (select distinct clientes.id from clientes) LOOP
+		tableName := format('transacoes_%s', idCliente);
+		EXECUTE format('
+			DELETE FROM %s
+			WHERE id < (
+				SELECT id
+				FROM %s
+				ORDER BY id DESC
+				LIMIT 1 OFFSET 11
+			);
+		', tableName, tableName);
+	END LOOP;
+END $$;
